22094c961d8b720a8f890137c2c365b4698c1a58
RishabhKumar2201 <rishabhkumar2204@gmail.com>
2024-07-04 01:56:46 +0530

    Implemented last message and time display on chat user card on home screen

--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chat_app/api/apis.dart';
+import 'package:chat_app/helper/my_date_util.dart';
 import 'package:chat_app/models/chat_user.dart';
+import 'package:chat_app/models/message.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +11,6 @@ import '../main.dart';
 
 //Card to represent a single user on Home Screen
 class ChatUserCard extends StatefulWidget {
-
   final ChatUser user;
 
   const ChatUserCard({super.key, required this.user});
@@ -18,51 +20,82 @@ class ChatUserCard extends StatefulWidget {
 }
 
 class _ChatUserCardState extends State<ChatUserCard> {
+  //last message info (if null --> no message)
+  Message? _message;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.symmetric(horizontal: mq.width * 0.02 , vertical: 4),
+      margin: EdgeInsets.symmetric(horizontal: mq.width * 0.02, vertical: 4),
       elevation: 1,
       //color: Colors.blue.shade100,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       child: InkWell(
-        onTap: () {
-          //for navigating to chat screen
-          Navigator.push(context, MaterialPageRoute(builder: (_) => ChatScreen(user: widget.user)));
-        },
-        child: ListTile(
-          //user profile picture
-          leading: /*CircleAvatar(child: Icon(CupertinoIcons.person))*/
-          ClipRRect(
-            borderRadius: BorderRadius.circular(mq.height * 0.40),
-            child: CachedNetworkImage(
-              fit: BoxFit.fill,
-              height: mq.height * 0.050,
-              width: mq.width * 0.095,
-              imageUrl: widget.user.image,
-              //placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => CircleAvatar(child: Icon(CupertinoIcons.person)),
-            ),
-          ),
-          //user name
-          title: Text(widget.user.name),
-          //last message
-          subtitle: Text(widget.user.about, maxLines: 1,),
-          //last message time
-          trailing: Container(
-            width: 25,
-            height: 25,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12.5),
-              color: Colors.green.shade400
-            ),
-          )
-          // Text('12:00 PM', style: TextStyle(
-          //     color: Colors.black54
-          // ),),
-        ),
-      ),
+          onTap: () {
+            //for navigating to chat screen
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (_) => ChatScreen(user: widget.user)));
+          },
+          child: StreamBuilder(
+            stream: APIs.getAllMessages(widget.user),
+            builder: (context, snapshot) {
+              final data = snapshot.data?.docs;
+              final list =
+                  data?.map((e) => Message.fromJson(e.data())).toList() ?? [];
+
+              if (list.isNotEmpty) {
+                _message = list[0];
+              }
+
+              return ListTile(
+                  //user profile picture
+                  leading: /*CircleAvatar(child: Icon(CupertinoIcons.person))*/
+                      ClipRRect(
+                    borderRadius: BorderRadius.circular(mq.height * 0.40),
+                    child: CachedNetworkImage(
+                      fit: BoxFit.fill,
+                      height: mq.height * 0.050,
+                      width: mq.width * 0.095,
+                      imageUrl: widget.user.image,
+                      //placeholder: (context, url) => CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          CircleAvatar(child: Icon(CupertinoIcons.person)),
+                    ),
+                  ),
+                  //user name
+                  title: Text(widget.user.name),
+                  //last message
+                  subtitle: Text(
+                    _message != null ? _message!.msg : widget.user.about,
+                    maxLines: 1,
+                  ),
+
+                  //last message time
+                  trailing: _message == null
+                      ? null //show nothing when no message is sent
+                      : _message!.read.isEmpty &&
+                              _message!.fromId != APIs.user.uid
+                          ?
+
+                          //show for unread message
+                          Container(
+                              width: 25,
+                              height: 25,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12.5),
+                                  color: Colors.green.shade400),
+                            )
+                          :
+                          //message sent time
+                          Text(
+                              MyDateUtil.getLastMessageTime(
+                                  context: context, time: _message!.sent),
+                              style: TextStyle(color: Colors.black54),
+                            ));
+            },
+          )),
     );
   }
 }
